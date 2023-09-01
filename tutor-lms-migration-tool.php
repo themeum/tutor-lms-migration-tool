963e7d1e246528ad50b50a5f020b1872cc0755a5
Mohammad Ibrahim <ikardi04@gmail.com>
2023-09-01 11:30:49 +0600

    version and download link updated

--- tutor-lms-migration-tool.php
+++ tutor-lms-migration-tool.php
@@ -4,7 +4,7 @@ Plugin Name: Tutor LMS - Migration Tool
 Plugin URI: https://www.themeum.com/
 Description: A migration toolkit that allows you to migrate data from other LMS platforms to Tutor LMS.
 Author: Themeum
-Version: 2.1.0
+Version: 2.2.0
 Author URI: http://themeum.com
 Requires at least: 5.3
 Tested up to: 6.0.2
@@ -25,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @since v.1.0.0
  */
 
-define('TLMT_VERSION', '2.1.0');
+define('TLMT_VERSION', '2.2.0');
 define('TLMT_FILE', __FILE__);
 define('TLMT_PATH', plugin_dir_path( TLMT_FILE ));
 define('TLMT_URL', plugin_dir_url( TLMT_FILE ));
