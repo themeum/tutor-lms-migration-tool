2293c4e49363f32608c1a406b3d9fa455daa39f7
shewa12 <shewa12kpi@gmail.com>
2025-06-23 15:43:57 +0600

    Course Meta: Course meta migration core methods added

--- inc/LDMigration/PostMeta/CourseMeta.php
+++ inc/LDMigration/PostMeta/CourseMeta.php
@@ -11,6 +11,7 @@
 namespace Themeum\TutorLMSMigrationTool\LDMigration\PostMeta;
 
 use Themeum\TutorLMSMigrationTool\Interfaces\PostMeta;
+use Tutor\Helpers\QueryHelper;
 
 /**
  * Handle course meta migration
@@ -35,9 +36,13 @@ class CourseMeta implements PostMeta {
 	 *
 	 * @param integer $post_id Post id.
 	 *
+	 * @throws \Throwable If Database error occur.
+	 *
 	 * @return void
 	 */
 	public function migrate( int $post_id ) {
+		global $wpdb;
+
 		$this->post_id = $post_id;
 
 		$migrate_able_meta = $this->get_migrate_able_meta();
@@ -45,6 +50,13 @@ class CourseMeta implements PostMeta {
 		if ( ! empty( $migrate_able_meta ) ) {
 			// Prepare meta.
 			$meta = $this->ld_to_tutor_meta_map( $migrate_able_meta );
+			if ( is_array( $meta ) && count( $meta ) ) {
+				try {
+					QueryHelper::insert_multiple_rows( $wpdb->postmeta, $meta, true, false );
+				} catch ( \Throwable $th ) {
+					throw $th;
+				}
+			}
 		}
 	}
 
@@ -90,24 +102,80 @@ class CourseMeta implements PostMeta {
 	 *
 	 * @param array $meta Mapped meta, ready to migrate.
 	 *
-	 * @return array
+	 * @return array Multi-dimension array with meta key and value.
 	 */
 	private function ld_to_tutor_meta_map( array $meta ): array {
 		$ld_tutor_meta_map = array(
 			'_tutor_course_price_type'        => 'sfwd-courses_course_price_type',
 			'_tutor_course_material_includes' => 'sfwd-courses_course_materials',
-			'tutor_course_sale_price'         => 'sfwd-courses_course_price_type',
+			'tutor_course_regular_price'      => 'sfwd-courses_course_price',
 		);
 
-		$ld_keys = array_values( $ld_tutor_meta_map );
+		$course_settings = array();
+		$has_tutor_pro   = function_exists( 'tutor_pro' );
+
+		if ( $has_tutor_pro ) {
+			$course_settings = array(
+				'maximum_students'     => 0,
+				'enrollment_expiry'    => 0,
+				'enable_content_drip'  => 0,
+				'content_drip_type'    => '',
+				'enrollment_starts_at' => '',
+				'enrollment_ends_at'   => '',
+				'pause_enrollment'     => '',
+			);
+		}
 
 		$tutor_meta_map = array();
-		foreach ( $meta as $key => $value ) {
-			if ( in_array( $key, $ld_keys ) ) {
-				$tutor_meta_map[ $ld_tutor_meta_map[ $key ] ] = $value;
+
+		$ld_keys = array_keys( $meta );
+		foreach ( $ld_tutor_meta_map as $key => $value ) {
+			if ( in_array( $value, $ld_keys ) ) {
+				$tutor_meta_map[ $key ] = $meta[ $value ];
 			}
 		}
 
-		return $tutor_meta_map;
+		// Price type.
+		$price_type = $tutor_meta_map['_tutor_course_price_type'];
+		if ( 'open' === $price_type ) {
+			$tutor_meta_map['_tutor_course_price_type'] = 'free';
+			// Set visibility public.
+			$tutor_meta_map['_tutor_is_public_course'] = 'yes';
+		} elseif ( 'paynow' === $price_type ) {
+			$tutor_meta_map['_tutor_course_price_type'] = 'paid';
+		} elseif ( 'closed' === $price_type && $has_tutor_pro ) {
+			$course_settings['pause_enrollment'] = 'yes';
+		} else {
+			$tutor_meta_map['_tutor_course_price_type'] = 'free';
+		}
+
+		if ( $has_tutor_pro ) {
+			// Max students.
+			if ( ! empty( $meta['sfwd-courses_course_seats_limit'] ) ) {
+				$course_settings['maximum_students'] = (int) $meta['sfwd-courses_course_seats_limit'];
+			}
+
+			// Enrollment expiry.
+			if ( ! empty( $meta['sfwd-courses_expire_access'] && 'on' === $meta[ $meta['sfwd-courses_expire_access'] ] ) ) {
+				$course_settings['enrollment_expiry'] = (int) $meta['sfwd-courses_expire_access_days'];
+			}
+		}
+
+		// Add course settings in meta.
+		$tutor_meta_map['_tutor_course_settings'] = maybe_serialize( $course_settings );
+
+		// Prepare to post meta to make it insert-able.
+		$prepared_meta = array();
+		foreach ( $tutor_meta_map as $key => $value ) {
+			$new_meta = array(
+				'post_id'    => $this->post_id,
+				'meta_key'   => $key,
+				'meta_value' => $value,
+			);
+
+			$prepared_meta[] = $new_meta;
+		}
+
+		return $prepared_meta;
 	}
 }
