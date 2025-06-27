432ce303178ba5681c8978b8e26b2c93c8300e4d
shewa12 <shewa12kpi@gmail.com>
2025-06-27 16:02:41 +0600

    Assignment Meta: Assignment meta migration script added

--- inc/Factories/PostMetaFactory.php
+++ inc/Factories/PostMetaFactory.php
@@ -13,6 +13,7 @@ namespace Themeum\TutorLMSMigrationTool\Factories;
 use InvalidArgumentException;
 use Themeum\TutorLMSMigrationTool\ContentTypes;
 use Themeum\TutorLMSMigrationTool\Interfaces\PostMeta;
+use Themeum\TutorLMSMigrationTool\LDMigration\PostMeta\AssignmentMeta;
 use Themeum\TutorLMSMigrationTool\LDMigration\PostMeta\CourseMeta;
 use Themeum\TutorLMSMigrationTool\LDMigration\PostMeta\LessonMeta;
 use Themeum\TutorLMSMigrationTool\LDMigration\PostMeta\QuizMeta;
@@ -45,6 +46,8 @@ abstract class PostMetaFactory {
 						return new LessonMeta();
 					case ContentTypes::QUIZ_META:
 						return new QuizMeta();
+					case ContentTypes::ASSIGNMENT_META:
+						return new AssignmentMeta();
 					default:
 						break;
 				}
