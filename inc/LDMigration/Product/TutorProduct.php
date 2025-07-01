a36b0b5413aedf4e9ce9db47f133d546dd2fffa6
Anindra Das Bivas <bivasdas911@gmail.com>
2025-07-01 13:15:06 +0600

    update format

--- inc/LDMigration/Product/TutorProduct.php
+++ inc/LDMigration/Product/TutorProduct.php
@@ -17,7 +17,7 @@ use Themeum\TutorLMSMigrationTool\Interfaces\Product;
 
 class TutorProduct implements Product {
 
-    /**
+	/**
 	 * Migration method to migrate from learndash to tutor product.
 	 *
 	 * @since 2.3.0
@@ -27,15 +27,14 @@ class TutorProduct implements Product {
 	 *
 	 * @return void
 	 */
-    public function migrate( int $course_id, string $course_title )
-    {
-        $course_details = get_post_meta( $course_id, '_sfwd-courses', true );
+	public function migrate( int $course_id, string $course_title ) {
+		$course_details = get_post_meta( $course_id, '_sfwd-courses', true );
 		update_post_meta( $course_id, '_tutor_course_price_type', 'free' );
 
-        if ( $course_details['sfwd-courses_course_price'] ) {
-            update_post_meta( $course_id, '_tutor_course_price_type', 'paid' );
-            update_post_meta( $course_id, 'tutor_course_price', $course_details['sfwd-courses_course_price'] );
-            update_post_meta( $course_id, 'tutor_course_sale_price', 0 );
-        }
-    }
+		if ( $course_details['sfwd-courses_course_price'] ) {
+			update_post_meta( $course_id, '_tutor_course_price_type', 'paid' );
+			update_post_meta( $course_id, 'tutor_course_price', $course_details['sfwd-courses_course_price'] );
+			update_post_meta( $course_id, 'tutor_course_sale_price', 0 );
+		}
+	}
 }
