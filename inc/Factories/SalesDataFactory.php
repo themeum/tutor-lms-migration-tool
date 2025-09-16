352154ae3280c48a5b68dd180dc1082582e358a4
shewa12 <shewa12kpi@gmail.com>
2025-09-16 13:44:58 +0600

    SaleDataTypes class added that contains data type const

--- inc/Factories/SalesDataFactory.php
+++ inc/Factories/SalesDataFactory.php
@@ -5,16 +5,17 @@
  * @package TutorLMSMigrationTool
  * @author Themeum <support@themeum.com>
  * @link https://themeum.com
- * @since 2.3.0
+ * @since 2.4.0
  */
 
 namespace Themeum\TutorLMSMigrationTool\Factories;
 
 use InvalidArgumentException;
-use Themeum\TutorLMSMigrationTool\ContentTypes;
 use Themeum\TutorLMSMigrationTool\Interfaces\MigrationTemplate;
 use Themeum\TutorLMSMigrationTool\MigrationTypes;
 use Themeum\TutorLMSMigrationTool\SalesData\WooToNative\Orders\Orders;
+use Themeum\TutorLMSMigrationTool\SalesData\WooToNative\Orders\Subscriptions;
+use Themeum\TutorLMSMigrationTool\SalesDataTypes;
 
 /**
  * OrderFactory class which gives order migration object based on monetization type.
@@ -24,20 +25,26 @@ abstract class SalesDataFactory {
 	/**
 	 * Create object for sales data
 	 *
-	 * @since 2.3.0
+	 * @since 2.4.0
 	 *
-	 * @param string $content_type   Type of content (order, subscription etc).
+	 * @param string $data_type   Type of content (order, subscription etc).
 	 * @param string $migration_type Type of migration (wc_to_native).
 	 *
 	 * @throws InvalidArgumentException If invalid argument passed.
 	 *
 	 * @return MigrationTemplate
 	 */
-	public static function create( string $content_type, string $migration_type ): MigrationTemplate {
+	public static function create( string $data_type, string $migration_type ): MigrationTemplate {
 		switch ( $migration_type ) {
 			case MigrationTypes::WC_TO_NATIVE:
-				if ( ContentTypes::SALES_ORDER === $content_type ) {
+				if ( SalesDataTypes::ORDERS === $data_type ) {
 					return new Orders();
+				} elseif ( SalesDataTypes::SUBSCRIPTIONS === $data_type ) {
+					return new Subscriptions();
+				} elseif ( SalesDataTypes::CUSTOMERS === $data_type ) {
+					return new Subscriptions();
+				} elseif ( SalesDataTypes::COUPONS === $data_type ) {
+					return new Subscriptions();
 				}
 				break;
 			default:
---
+++ inc/SalesDataTypes.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Sales data types
+ *
+ * @package TutorLMSMigrationTool
+ * @author Themeum <support@themeum.com>
+ * @link https://themeum.com
+ * @since 2.4.0
+ */
+
+namespace Themeum\TutorLMSMigrationTool;
+
+/**
+ * Contains constant for the available migration types.
+ *
+ * @since 2.3.0
+ */
+abstract class SalesDataTypes {
+
+	const ORDERS        = 'orders';
+	const COUPONS       = 'coupons';
+	const SUBSCRIPTIONS = 'subscriptions';
+	const CUSTOMERS     = 'customers';
+}
