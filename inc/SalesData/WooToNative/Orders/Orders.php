79d9a095493af38055f24ff82eafeebb41b2b8b0
shewa12 <shewa12kpi@gmail.com>
2025-09-30 11:58:47 +0600

    Fix: Orders fetching all the orders instead of just shop order & shop order refund

--- inc/SalesData/WooToNative/Orders/Orders.php
+++ inc/SalesData/WooToNative/Orders/Orders.php
@@ -142,6 +142,10 @@ class Orders implements MigrationTemplate {
 				'limit'      => $limit,
 				'offset'     => $offset,
 				'status'     => $this->get_order_statuses(),
+				'type'       => array(
+					'shop_order',
+					'shop_order_refund',
+				),
 				'orderby'    => 'date',
 				'order'      => 'DESC',
 				'return'     => 'objects',
@@ -169,6 +173,10 @@ class Orders implements MigrationTemplate {
 			array(
 				'status'     => $this->get_order_statuses(),
 				'return'     => 'ids',
+				'type'       => array(
+					'shop_order',
+					'shop_order_refund',
+				),
 				'limit'      => -1,
 				'meta_query' => array(
 					array(
